1f8db90bd2da2530cf40e3392c8f2e91dff14f39
Tom Prince <thomas.prince@common.com>
2020-04-02 12:33:01 -0400

    add comments

--- super-cool.js
+++ super-cool.js
@@ -1,3 +1,15 @@
 function coolFunctionBro() {
     return null;
 }
+
+// Starting point
+// master  A - B - C
+// cooler-function  \- D - E
+
+// master  A - B - C
+// coolest-function \- F - G 
+
+// What we want
+// master  A - B - C
+// cooler-function  \- D - E
+// coolest-function         \- F - G
